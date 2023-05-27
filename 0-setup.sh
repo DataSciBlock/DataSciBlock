bd407609c675b7b46806463ea8063cc22fba3d37
Bunyasit Fang <wachaa1319@gmail.com>
2023-05-27 23:32:21 +0700

    setup

--- 0-setup.sh
+++ 0-setup.sh
@@ -18,7 +18,7 @@ echo $DIR
 
 echo "Adding extension to Scratch source"
 cd $SCRATCH_SRC_HOME/scratch-vm/src/extensions
-ln -s $DIR/your-scratch-extension your-scratch-extension
+ln -s $DIR/scratch3_datasci scratch3_datasci
 
 echo "Patching Scratch source to enable extension"
 cd $SCRATCH_SRC_HOME/scratch-vm
@@ -31,10 +31,10 @@ cd $SCRATCH_SRC_HOME/scratch-gui
 git apply $DIR/patches/scratch-gui.patch
 
 echo "Copying in the Scratch extension files"
-mkdir -p src/lib/libraries/extensions/yourextension
-cd src/lib/libraries/extensions/yourextension
-ln -s $DIR/your-extension-background.png your-extension-background.png
-ln -s $DIR/your-extension-icon.png your-extension-icon.png
+mkdir -p src/lib/libraries/extensions/datasci
+cd src/lib/libraries/extensions/datasci
+ln -s $DIR/datasci.png datasci.png
+ln -s $DIR/datasci-icon.svg datasci-icon.svg
 
 echo "Marking the Scratch source as customized"
 touch $SCRATCH_SRC_HOME/patched
