d84b18a232888b20212c76f6f7e17ae86cc1bc40
Bunyasit Fang <wachaa1319@gmail.com>
2023-05-29 13:39:45 +0700

    feat: auto add env

--- 1-add-dependency.sh
+++ 1-add-dependency.sh
@@ -3,6 +3,14 @@ set -e
 
 MODULE=$1
 
+SCRATCH_GUI="scratch-gui"
+SCRATCH_VM="scratch-vm"
+
+if [ -d "$SCRATCH_VM" ] && [ -d "$SCRATCH_GUI" ]; then 
+    echo "setting environment variable"
+    export SCRATCH_SRC_HOME=$(pwd)
+fi
+
 echo "Verifying location of Scratch source is known"
 if [ -z "$SCRATCH_SRC_HOME" ]; then
     echo "Error: SCRATCH_SRC_HOME environment variable is not set."
