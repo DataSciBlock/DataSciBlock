a359a6b2ecadf294bd272dcd7394f35faec5311f
Bunyasit Fang <wachaa1319@gmail.com>
2023-05-29 13:37:41 +0700

    feat: build and run with -b

--- 3-run-private.sh
+++ 3-run-private.sh
@@ -8,6 +8,18 @@ if [ -d "$SCRATCH_VM" ] && [ -d "$SCRATCH_GUI" ]; then
     echo "setting environment variable"
     export SCRATCH_SRC_HOME=$(pwd)
 fi
+
+
+# Loop through the positional parameters
+for arg in "$@"; do
+    if [ "$arg" = "--build-vm" ] || [ "$arg" = "-b" ];  then
+        echo "BUILDING SCRATCH VM ..."
+        cd "$SCRATCH_SRC_HOME/scratch-vm"
+        npm run build
+        break
+    fi
+done
+
 echo "STARTING SCRATCH GUI AT http://localhost:8601"
-cd $SCRATCH_SRC_HOME/scratch-gui
+cd "$SCRATCH_SRC_HOME/scratch-gui"
 npm start
